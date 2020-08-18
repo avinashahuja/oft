f87f673b81134de170c61f0b1875778dd478ff83
Avinash Ahuja <me@avinashahuja.me>
2020-08-18 09:53:31 -0700

    Updated run.sh to enable multi-gpu utilization

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 #Install requirements
+apt-get install htop unzip vim
 pip install -r requirements.txt
 
 # Run training
 EXPERIMENT_NAME="train-benchmark-8gpu"
-python train.py $EXPERIMENT_NAME --gpu 0 > $EXPERIMENT_NAME.out &
+EXPT_BATCH_SIZE=50
+EXPT_NUM_DATA_LOADERS=32
+python train.py $EXPERIMENT_NAME --gpu 0 1 2 3 4 5 6 7 -b $EXPT_BATCH_SIZE -w $EXPT_NUM_DATA_LOADERS > $EXPERIMENT_NAME.out &
 tensorboard --logdir experiments/$EXPERIMENT_NAME
